b07896bde5a371a6aecc06a3cdbc10cb73306288
oncet <camilorivera86@gmail.com>
2021-01-24 04:36:41 -0300

    display product

---
+++ pages/product/[id].js
@@ -0,0 +1,41 @@
+import { useRouter } from "next/router";
+import prisma from "../../lib/prisma";
+
+export async function getStaticPaths() {
+  const products = await prisma.product.findMany({
+    select: { id: true },
+  });
+
+  const paths = products.map(({ id }) => ({
+    params: { id: id.toString() },
+  }));
+
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+export async function getStaticProps({ params }) {
+  const product = await prisma.product.findUnique({
+    where: {
+      id: Number(params.id),
+    },
+  });
+
+  return {
+    props: {
+      product,
+    },
+  };
+}
+
+const Product = ({ product }) => {
+  const router = useRouter();
+  if (router.isFallback) {
+    return <p>Loading...</p>;
+  }
+  return <p>Product: {product.name}</p>;
+};
+
+export default Product;
