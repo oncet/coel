77507cb05e4cc4fca88c58e4ff1c9a5787cb0e0c
oncet <camilorivera86@gmail.com>
2021-01-24 04:36:03 -0300

    prisma setup

---
+++ prisma/migrations/20210123033721_create_product_table/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "slug" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "price" INTEGER,
+
+    PRIMARY KEY ("id")
+);
