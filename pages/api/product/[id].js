1ebf991c424e7e6b508c2326c5a337ebb8e87e03
oncet <camilorivera86@gmail.com>
2021-02-01 00:01:26 -0300

    update product api endpoint

--- pages/api/product/[id].js
+++ pages/api/product/[id].js
@@ -1,19 +1,24 @@
-const putHandler = (req, res) => {
+import prisma from "../../../lib/prisma";
+
+const putHandler = async (req, res) => {
   const { id } = req.query;
-  console.log(req.body);
+  await prisma.product.update({
+    where: { id: Number(id) },
+    data: req.body,
+  });
   res.statusCode = 200;
-  res.json({ name: "John Doe!!!", id });
+  res.end();
 };
 
 const methodHandlers = {
   PUT: putHandler,
 };
 
-export default (req, res) => {
+export default async (req, res) => {
   if (methodHandlers.hasOwnProperty(req.method)) {
     methodHandlers[req.method](req, res);
+  } else {
+    res.statusCode = 404;
+    res.end();
   }
-
-  res.statusCode = 404;
-  res.end();
 };
