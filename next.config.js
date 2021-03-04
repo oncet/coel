26598ba3cb1affa55a33b838604999c69fefb7a3
oncet <camilorivera86@gmail.com>
2021-03-04 02:18:21 -0300

    i18n setup

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 const withTM = require("next-transpile-modules")(["ky"]);
 
-module.exports = withTM();
+module.exports = withTM({
+  i18n: {
+    locales: ["en", "es"],
+    defaultLocale: "en",
+    localeDetection: false,
+  },
+});
