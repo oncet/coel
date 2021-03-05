bc8d23938dba55d009334f8e9a938edef0d10327
oncet <camilorivera86@gmail.com>
2021-03-05 01:38:44 -0300

    block field style change

--- components/block-field.js
+++ components/block-field.js
@@ -2,17 +2,17 @@ import { Field as FormikField } from "formik";
 
 export default function BlockField({ label, name, errors, touched, ...rest }) {
   return (
-    <>
+    <div className="mb-3 ">
       <label className="block mb-1" htmlFor={name}>
         {label}
       </label>
       <FormikField
-        className="border border-gray-400 rounded w-full block mb-2.5 text-black"
+        className="border border-gray-400 rounded w-full block text-black"
         id={name}
         name={name}
         {...rest}
       />
       {!!(errors && touched) && <div>{errors}</div>}
-    </>
+    </div>
   );
 }
