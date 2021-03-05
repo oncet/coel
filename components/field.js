d9400189874e1016e13c6a1542dc14609c037705
oncet <camilorivera86@gmail.com>
2021-03-05 01:38:54 -0300

    add children to field component

--- components/field.js
+++ components/field.js
@@ -1,12 +1,21 @@
 import { Field as FormikField } from "formik";
 
-export default function Field({ label, name, errors, touched, ...rest }) {
+export default function Field({
+  children,
+  label,
+  name,
+  errors,
+  touched,
+  ...rest
+}) {
   return (
-    <div className="flex mb-2">
+    <div className="flex items-center mb-3">
       <label className="mr-2" htmlFor={name}>
         {label}
       </label>
-      <FormikField id={name} name={name} {...rest} />
+      <FormikField id={name} name={name} {...rest}>
+        {children}
+      </FormikField>
       {!!(errors && touched) && <div>{errors}</div>}
     </div>
   );
