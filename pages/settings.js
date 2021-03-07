e18841c5b3f7d75213647f3a071ce56bed1f1d13
oncet <camilorivera86@gmail.com>
2021-03-07 20:43:46 -0300

    move moon icon to separated file

--- pages/settings.js
+++ pages/settings.js
@@ -7,6 +7,7 @@ import * as Yup from "yup";
 
 import Field from "../components/field";
 import Button from "../components/button";
+import MoonIcon from "../components/icons/moon";
 
 const Settings = () => {
   const { theme, setTheme } = useTheme();
@@ -51,15 +52,7 @@ const Settings = () => {
             <Field
               label={
                 <div className="flex">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 20 20"
-                    fill="currentColor"
-                    className="w-5 mr-2"
-                  >
-                    <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z" />
-                  </svg>{" "}
-                  Dark mode
+                  <MoonIcon /> Dark mode
                 </div>
               }
               name="darkMode"
