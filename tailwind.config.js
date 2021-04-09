2aae534c94c8b84020cd4c9f24726db2fa2ae46c
oncet <camilorivera86@gmail.com>
2021-04-09 00:37:42 -0300

    use tailwind jit mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,9 @@
 module.exports = {
   purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  mode: "jit",
   darkMode: "class", // or 'media' or 'class'
   theme: {
     extend: {},
-  },
-  variants: {
-    extend: {
-      opacity: ["disabled"],
-      display: ["group-hover"],
-    },
   },
   plugins: [require("@tailwindcss/forms")],
 };
