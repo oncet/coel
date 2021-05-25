846bce2bdf969097011f7cc9e568af115920f91c
oncet <camilorivera86@gmail.com>
2021-05-25 15:46:07 -0300

    add image migration

---
+++ prisma/migrations/20210525184430_create_image_table/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "Image" (
+    "id" SERIAL NOT NULL,
+    "originalName" TEXT NOT NULL,
+    "fileName" TEXT NOT NULL,
+    "path" TEXT NOT NULL,
+    "alt" TEXT NOT NULL,
+    "productId" INTEGER NOT NULL,
+
+    PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Image" ADD FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
