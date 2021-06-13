e4aaf6ba7b025a98d7b04f958c38d8e3c7ce3079
oncet <camilorivera86@gmail.com>
2021-06-12 23:42:45 -0300

    change button default type to button

--- components/button.js
+++ components/button.js
@@ -2,7 +2,7 @@ export default function Button({ children, className, ...rest }) {
   return (
     <button
       className={`bg-indigo-600 rounded px-2 py-1 inline-block w-full disabled:opacity-50 text-white ${className}`}
-      type="submit"
+      type="button"
       {...rest}
     >
       {children}
